39de7d300062210a050422c0c48d7cb01e1450b8
zbay <zlwilliams1@gmail.com>
2016-05-24 06:16:33 +0000

    Replaced class with className

--- src/components/StatelessUI/Footer.jsx
+++ src/components/StatelessUI/Footer.jsx
@@ -3,9 +3,9 @@ var React = require('react');
 module.exports = React.createClass({
     render: function(){
         return (<div id="footer">
-        <div class="VertCenterTop"></div>
+        <div className="VertCenterTop"></div>
        <div>&copy; 2016 - Zbay Industries</div>
-       <div class="VertCenterBottom"></div>
+       <div className="VertCenterBottom"></div>
         </div>);
     }
 });
