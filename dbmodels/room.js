6ceee7e2aab6cd7fe59f54336094b2d3af0ee8e8
zbay <zlwilliams1@gmail.com>
2016-05-26 18:46:43 +0000

    Including TTL index in room schema

--- dbmodels/room.js
+++ dbmodels/room.js
@@ -3,4 +3,6 @@ var Schema = mongoose.Schema;
 var RoomSchema = new Schema({"name": {type: String, required: true}, "expiration": Date, "password": String,
 "messages": [{"text": {type: String, required: true}, "author": String, "timePosted": {type: Date, default: Date.now}}]});
 
+RoomSchema.index({"expiration": 1}, {expireAfterSeconds: 43200});
+
 mongoose.model('Room', RoomSchema);
